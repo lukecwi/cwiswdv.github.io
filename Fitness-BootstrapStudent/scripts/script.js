3a2e70c716efaf9665f57d2465b709b57e18ec65
Luke CWI <73607944+lukecwi@users.noreply.github.com>
2020-11-30 22:57:23 -0700

    adding everything

---
+++ Fitness-BootstrapStudent/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Luke Chulack
+    File Name: script.js
+    Date: 11/30/2020
+*/
+
+//JQuery for herp image  to consune the header window space
+$(document).ready(function() {
+   $('.hero').height($(window).height());  
+});
