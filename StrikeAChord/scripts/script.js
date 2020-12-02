12f743690e3c7f03aafc5497233e3af957815290
Luke CWI <73607944+lukecwi@users.noreply.github.com>
2020-12-02 12:14:47 -0700

    all of it.

---
+++ StrikeAChord/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Luke Chulack
+    File Name: script.js
+    Date: 12/2/2020
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
