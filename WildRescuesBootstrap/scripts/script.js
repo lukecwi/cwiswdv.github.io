cd5694d1dfae48e89075f60e781a7257edeee22b
Luke CWI <73607944+lukecwi@users.noreply.github.com>
2020-12-02 23:44:47 -0700

    all of it.

---
+++ WildRescuesBootstrap/scripts/script.js
@@ -0,0 +1,11 @@
+/*
+    Student Name: Luke Chulack
+    File Name: script.js
+    Date: 12/2/2020
+*/
+
+
+//jQuery document ready event to the file
+$(document).ready(function(){
+   $('.hero').height($(window).height());
+});
